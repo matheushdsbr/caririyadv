42e4b3bcc46015c405ba5dcec8e6be30f74fda3a
eriick505 <erick-jeck@hotmail.com>
2020-07-30 18:03:05 -0300

    modificações para PHP

---
+++ components/banner.php
@@ -0,0 +1,15 @@
+<!-- BANNER -->
+<div class="row">
+    <div class="col-12 mt-4">
+        <div class="banner pl-md-5 pl-2">
+            <div class="overflow"></div>
+
+            
+            <div class="banner__text">
+                <?php include('functions/title-page-banner.php'); ?>
+                <!-- <span>Seja bem-vindo!</span>
+                <h2>Advocacia e Consultoria</h2> -->
+            </div>
+        </div>
+    </div>
+</div>
---
+++ paginas/home.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mb-md-0 mb-3">
+            <!-- principles and importance -->
+            <section class="principles">
+                <h2>Nossos <b>Princípios</b> e <b>Valores</b></h2>
+
+                <p>Colocamos em primeiro lugar os valores que regem nossas ações: Excelência, Seriedade e Confiabilidade. Esses valores garantem não apenas um trabalho consolidado, mas também exprimem a preocupação e constante busca de superar as expectativas de nossos clientes.</p>
+                <p>A principal marca consiste na interação da justiça com as necessidades e anseios dos nossos clientes. Desta forma, buscamos soluções jurídicas inovadoras, eficientes, eficazes, efetivas, dinâmicas e corporativas.</p>
+                <p>Os resultados positivos obtidos são frutos de um trabalho consubstanciado em competência e sigilo profissional para com os clientes, bem como em lealdade e transparência na efetivação dos nossos serviços.</p>
+
+                <a href="#" class="principles__button" title="página sobre a advogada Natalia Cariry">Saiba mais <i class="fas fa-arrow-right"></i></a>
+            </section>
+        </div>
+
+        <div class="col-md-4">
+            <?php include('components/sidebar.html'); ?>
+        </div>
+    </div>
+</div>
